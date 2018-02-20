64340c75407d382dc0b80a213bcc933137f733e9
王子昂 <gnaizgnaw@163.com>
2018-02-20 15:50:49 +0800

    add User.java

---
+++ User.java
@@ -0,0 +1,26 @@
+package com.po;
+
+public class User {
+    private String username;
+    private String password;
+    //保留默认构造函数
+    public User(){
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
